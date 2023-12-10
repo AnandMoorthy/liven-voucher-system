c1bc734b8c5654bed484a31dd46deed5a44fd2db
Anandha Moorthy <anandmoorthyj@gmail.com>
2023-12-11 04:24:49 +0530

    To commit

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,7 +1,7 @@
 import uuid
 from django.db import models
 
-# Create your models here.
+# Create your models here
 class Restaurant(models.Model):
     name = models.CharField(max_length=255, null=False) 
     created_at = models.DateField(auto_now_add=True)
